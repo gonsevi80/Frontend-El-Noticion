b7b6b059b78db7a32b1a9b254c819742c4fde2cc
Andrea Katherine <jaimeandreak@gmail.com>
2024-01-18 13:12:57 +0100

    agregando pages

---
+++ src/pages/NewNew.jsx
@@ -0,0 +1,11 @@
+import FormNewNew from "../components/FormNewNew";
+
+const NewNew = () => {
+  return (
+    <div>
+      <h3>Nueva Entrada Del Noticón</h3>
+      <FormNewNew />
+    </div>
+  );
+};
+export default NewNew;
