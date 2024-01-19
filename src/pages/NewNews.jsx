a9e7e8d1a4744c4ecd4b21bdbb9db72c122b855f
Andrea Katherine <jaimeandreak@gmail.com>
2024-01-19 23:42:40 +0100

    agregando dos pages que faltaban

---
+++ src/pages/NewNews.jsx
@@ -0,0 +1,11 @@
+import FormNewNews from "../components/FormNewNews";
+
+const NewNews = () => {
+  return (
+    <div>
+      <h3>Nueva Entrada De El Noticón</h3>
+      <FormNewNews />
+    </div>
+  );
+};
+export default NewNews;
