b42011a664a799f680f86a9b3c35aa2c7b470ca4
Juan de la Morena Marzalo <delamorenajuan@gmail.com>
2024-01-21 17:49:30 +0100

    cambios en los archivos de service

---
+++ src/context/AuthContextProvider.jsx
@@ -0,0 +1,41 @@
+import { createContext,useState, useEffect } from "react";
+import getDataUserLoggedService from "../service/getDataUserLoggedService";
+
+export const AuthContext = createContext();
+
+const AuthContextProvider = ({ children }) => {
+
+    const [token, setToken] = useState(localStorage.getItem('token'));
+
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        localStorage.setItem('token', token);
+    },[token]);
+    
+    useEffect(() => {
+        const getDataUserLogged = async () => {
+            try {
+                const data = await getDataUserLoggedService( {token} );
+                setUser(data);
+            } catch (error) {
+                logout();
+            }            
+        }
+        
+        getDataUserLogged();
+    }, [token]);
+    
+    const logout = () => {
+        setToken('');
+        setUser(null);
+    }
+
+    return (
+        <AuthContext.Provider value={{token, setToken, user, logout}}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContextProvider;
---
+++ src/service/modifyUserService.js
@@ -0,0 +1,23 @@
+
+
+const modifyUserService = async (data,token) => {
+    
+    const { VITE_API_URL } = import.meta.env;
+
+    const url = `${ VITE_API_URL }/users/update`;
+
+    const response = await fetch(url, {
+        method: 'PATCH',
+        headers:{
+            authorization: token
+        },
+        body: data
+    });
+
+    const json = await response.json();
+
+    if(!response.ok) throw new Error(json.message);
+
+}
+
+export default modifyUserService;
