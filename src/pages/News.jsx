381cbfb985ee9f0754d320f51bf00e2dac4f37e6
Natalia Arquero Fernandez <nataliarquero@gmail.com>
2024-01-22 18:16:11 +0100

    .

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,38 +1,40 @@
 import { useState, useEffect } from "react";
 import fetchApi from "../service/fetchApi";
 import { Link } from "react-router-dom";
-import useNews from "../hooks/useNews";
+import usenews from "../hooks/usenews";
 
 const News = () => {
   const { VITE_API_URL } = import.meta.env;
 
-  const newsData = useNews();
+  const news = useNews();
 
   return (
     <>
       <h2>Entradas El Notición</h2>
-      {/* Render condicional de un formulario */}
-      {Array.isArray(useNews) &&
-        newsData.map((news) => {
-          return (
-            <div key={news.id}>
-              <p>Noticia: {news.entrance}</p>
-              {news.photo.length ? (
-                <img
-                  src={`${VITE_API_URL}/uploads/${news.photos[0].name}`}
-                  alt=""
-                />
-              ) : (
-                "La noticia no contiene imágenes"
-              )}
-              <Link to={`/news/detail/${news.id}`}>
-                <p>Más info...</p>
-              </Link>
-            </div>
-          );
-        })}
+      {
+        /*render condicional de un formulario*/
+        news &&
+          news?.map((news) => {
+            return (
+              <div key={news.id}>
+                <p>Noticia: {news.entrance}</p>
+                {news.photo.length ? (
+                  <img
+                    src={`${VITE_API_URL}/uploads/${news.photos[0].name}`}
+                    alt=""
+                  />
+                ) : (
+                  "La noticia no contiene imagenes"
+                )}
+                <Link to={`/news/detail/${news.id}`}>
+                  <p>Mas info...</p>
+                </Link>
+              </div>
+            );
+          })
+      }
     </>
   );
 };
 
-export default News;
+export default news;
