c67c556809d4b9e39efa8c85b7b951d0f8e2acb3
gonsevi80 <142497836+gonsevi80@users.noreply.github.com>
2024-01-23 22:56:08 +0100

    Revert "Natalia"

--- src/App.jsx
+++ src/App.jsx
@@ -15,23 +15,21 @@ function App() {
   return (
     <>
       <Header />
-      <div id="root">
-        <Routes>
-          <Route path="/" element={<News />} />
-          <Route path="/users/login" element={<Login />} />
-          <Route path="/users/register" element={<Register />} />
-          <Route path="/news/detail/:newsId" element={<NewsDetail />} />
-          <Route path="/news/new-news" element={<NewNews />} />
-          <Route
-            path="/user/recover-password"
-            element={<RecoverPasswordPage />}
-          />
-          <Route path="/user/profile" element={<ProfilePage />}>
-            <Route path="/user/profile/modify" element={<FormUserEdit />} />
-          </Route>
-          <Route path="*" element={<News />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<News />} />
+        <Route path="/users/login" element={<Login />} />
+        <Route path="/users/register" element={<Register />} />
+        <Route path="/news/detail/:newsId" element={<NewsDetail />} />
+        <Route path="/news/new-news" element={<NewNews />} />
+        <Route
+          path="/user/recover-password"
+          element={<RecoverPasswordPage />}
+        />
+        <Route path="/user/profile" element={<ProfilePage />}>
+          <Route path="/user/profile/modify" element={<FormUserEdit />} />
+        </Route>
+        <Route path="*" element={<News />} />
+      </Routes>
       <Footer />
     </>
   );
