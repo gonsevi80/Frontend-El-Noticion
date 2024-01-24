861d4fa2df330237b587991b9f7478b899427a1d
Andrea Katherine <jaimeandreak@gmail.com>
2024-01-24 19:11:24 +0100

    volviendo a un commit anterior

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,11 @@ import AuthUser from "./AuthUser";
 import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/AuthContextProvider";
 import { useContext } from "react";
+<<<<<<< HEAD
 import styles from "/src/components_css/header.module.css";
+=======
+import "/src/styles/header.css";
+>>>>>>> bdd2e08 (volviendo a un commit anterior)
 
 const Header = () => {
   const { user } = useContext(AuthContext);
@@ -14,11 +18,14 @@ const Header = () => {
           <NavLink to={"/home"} className="nav-link-home">
             Inicio
           </NavLink>
+<<<<<<< HEAD
           {!user && (
             <NavLink to={"/news"} className="nav-link">
               Nueva noticia
             </NavLink>
           )}
+=======
+>>>>>>> bdd2e08 (volviendo a un commit anterior)
         </div>
         <div className="styles.right-links">
           {!user ? (
@@ -26,8 +33,14 @@ const Header = () => {
               <NavLink to={"/users/login"} className="nav-link">
                 Iniciar sesión
               </NavLink>
+<<<<<<< HEAD
               <NavLink to={"/users/register"} className="nav-link">
                 Registrarse
+=======
+
+              <NavLink to={"/register"} className="nav-link">
+                Registrarte
+>>>>>>> bdd2e08 (volviendo a un commit anterior)
               </NavLink>
             </>
           ) : (
