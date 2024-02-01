0804ce8211dd0e80e57426e44c962a1f05b245b0
Gonzalo Bulnes Menjibar <gonsevi80@gmail.com>
2024-02-01 16:48:05 +0100

    actualizando

---
+++ src/hooks/useNewsById.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import getNewsByIdService from "../service/getNewsByIdService";
+
+const useNewsById = (newsId) => {
+  const [news, setNews] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const getNewsById = async () => {
+      try {
+        const data = await getNewsByIdService(newsId);
+
+        setNews(data.data.news);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    getNewsById();
+  }, [newsId]);
+
+  return { news, error };
+};
+
+export default useNewsById;
