17569a319a06d1e18844c04c3feb3650e2100b6d
Juan de la Morena Marzalo <delamorenajuan@gmail.com>
2024-02-09 19:55:37 +0100

    psicko

--- src/components/DeleteConfirmationPopup.jsx
+++ src/components/DeleteConfirmationPopup.jsx
@@ -1,39 +1,34 @@
-// components/DeleteConfirmationPopup.js
 import { useState } from "react";
 import PropTypes from "prop-types";
+import styles from "../styles/DeleteConfirmationPopup.module.css";
 
 const DeleteConfirmationPopup = ({ onConfirm, onCancel }) => {
   const [username, setUsername] = useState("");
 
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        backgroundColor: "white",
-        padding: "20px",
-        zIndex: 100,
-      }}
-    >
+    <div className={styles.deleteConfirmation}>
       <h2>¿Estás seguro de que quieres caer en el olvido?</h2>
       <input
         type="text"
         placeholder="Escribe tu nombre de usuario para confirmar"
+        className={styles.input}
         value={username}
         onChange={(e) => setUsername(e.target.value)}
       />
       <br />
-      <button onClick={() => onConfirm(username)}>Confirmar</button>
-      <button onClick={onCancel}>Cancelar</button>
+      <button className={styles.button} onClick={() => onConfirm(username)}>
+        Confirmar
+      </button>
+      <button className={styles.button} onClick={onCancel}>
+        Cancelar
+      </button>
     </div>
   );
 };
 
 DeleteConfirmationPopup.propTypes = {
-  onConfirm: PropTypes.func.isRequired, // Indica que onConfirm es una función y es requerida
-  onCancel: PropTypes.func.isRequired, // Indica que onCancel es una función y es requerida
+  onConfirm: PropTypes.func.isRequired,
+  onCancel: PropTypes.func.isRequired,
 };
 
 export default DeleteConfirmationPopup;
