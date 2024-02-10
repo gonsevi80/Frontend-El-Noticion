08ead48a3c7c745cd9d112bc447005406e3992f1
Juan de la Morena Marzalo <delamorenajuan@gmail.com>
2024-02-10 12:31:16 +0100

    pasamos a formularios

--- src/pages/RecoverPasswordPage.jsx
+++ src/pages/RecoverPasswordPage.jsx
@@ -1,53 +1,9 @@
-import { useState } from "react";
-import setRecoverPasswordService from "../service/setRecoverPasswordService";
-import { useNavigate } from "react-router-dom";
+import RecoverForm from "../components/RecoverForm.jsx";
 
-const RecoverPasswordPage = ({ onClose }) => {
-  const [email, setEmail] = useState("");
-  const [username, setUserName] = useState("");
-  const [error, setError] = useState("");
-
-  const navigate = useNavigate();
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      await setRecoverPasswordService(email, username);
-      navigate("/users/password"); // Redirige al usuario después del proceso
-      if (onClose) onClose(); // Cierra el modal o la tarjeta si se proporcionó la función onClose
-    } catch (error) {
-      setError(error.message); // Maneja errores, por ejemplo, si el usuario o email no existen
-    }
-  };
-
+const RecoverPasswordPage = () => {
   return (
     <div>
-      <h3 className="Titulo">Recuperar contraseña</h3>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Nombre de usuario: </label>
-          <input
-            className="recuadro"
-            type="text"
-            name="username"
-            value={username}
-            onChange={(e) => setUserName(e.target.value)}
-          />
-          <label>Email: </label>
-          <input
-            className="recuadro"
-            type="email"
-            name="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="bot-ini">
-          Enviar
-        </button>
-        {error && <p>{error}</p>}
-      </form>
+      <RecoverForm />
     </div>
   );
 };
